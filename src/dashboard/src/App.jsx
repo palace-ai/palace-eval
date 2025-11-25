cad21dd7d067dbb41545b12ca44a32cd0779e1d5
Massimiliano Altieri <massimiliano.altieri@ec.europa.eu>
2025-11-25 14:57:17 +0100

    trying to add dashboard into palace

---
+++ src/dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import EvaluationDashboard from './components/EvaluationDashboard';
+
+function App() {
+  return (
+    <div className="app">
+      <EvaluationDashboard />
+    </div>
+  );
+}
+
+
+export default App;
