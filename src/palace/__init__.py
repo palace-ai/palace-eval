86e9abef4129449291633d40b44d0bf96ad34e46
Massimiliano Altieri <massimiliano.altieri@ec.europa.eu>
2026-05-07 15:12:40 +0200

    Fixed circular import

--- src/palace/__init__.py
+++ src/palace/__init__.py
@@ -1,1 +1,5 @@
-from .entrypoints.palace_run import evaluate
+def __getattr__(name):
+    if name == "evaluate":
+        from .entrypoints.palace_run import evaluate
+        return evaluate
+    raise AttributeError(f"module 'palace' has no attribute {name!r}")
